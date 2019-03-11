d898d9c2d80157b5863d1124f7c4271e6b7c9ca7
askari01 <farrukh.askari01@gmail.com>
2019-03-11 16:26:59 +0500

    test update

--- test/approvalContract.js
+++ test/approvalContract.js
@@ -5,5 +5,11 @@ contract('ApprovalCOntracts', function(accounts) {
         const contract = await ApprovalContract.deployed();
         const approver = await contract.approver.call();
         assert.equal(approver, "0x629a56C6ef6b5bda6091Aecac00540783dB561Bf", "approver doesn't match");
-    })
+    });
+
+    if('takes a deposit', async function() {
+        const contract = await ApprovalContract.deployed();
+        await contract.deposit(accounts[0], {value: 1e+18, from: accounts[1]});
+        assert.equal(web3.eth.getBalance(contract.address), 1e+18, "ammount did not match");
+    });
 })
